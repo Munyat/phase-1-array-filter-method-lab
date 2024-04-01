964824f9bce3697c4cdb96d300557e85e4692540
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-04-01 13:57:12 +0300

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(arrayDrivers, driverName) {
+  return arrayDrivers.filter(function (driver) {
+    return driver.toUpperCase() === driverName.toUpperCase();
+  });
+}
+
+function fuzzyMatch(arrayDrivers, driverName) {
+  const driver = arrayDrivers.filter((driver) =>
+    driver.toLowerCase().startsWith(driverName.toLowerCase())
+  );
+  return driver;
+}
+
+function matchName(arrayDrivers, driverName) {
+  const nameProperty = arrayDrivers.filter(
+    (driverObject) => driverObject.name === driverName
+  );
+  return nameProperty;
+}
